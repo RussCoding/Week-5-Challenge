002c60311da26ba33742a28c2521714d93db54ce
unknown <russelhkamal@gmail.com>
2022-04-11 06:28:05 -0400

    Added function to change color of textboxes with time

--- Develop/script.js
+++ Develop/script.js
@@ -3,3 +3,23 @@ $(document).ready(function() {
     var date = moment().format('dddd, MMM Do');
     $("#currentDay").text(date);
 });
+
+//sets a class for each hour block to change background color
+function checkTime() {
+    var currentHour = moment().hour();    
+    $(".row").each(function() {
+        var timeblock = parseInt($(this).attr("name"));
+        
+        if (currentHour > timeblock){
+            $(this).addClass("past");
+        }
+        else if (currentHour === timeblock){
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("future");
+        }
+    })
+}
+
+checkTime();
