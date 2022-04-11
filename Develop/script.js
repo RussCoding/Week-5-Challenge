72c793529daa84ec8f1be443b5262f4795ec4bec
unknown <russelhkamal@gmail.com>
2022-04-11 05:01:08 -0400

    Finished time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Shows the date in the header
 $(document).ready(function() {
-    var m = moment().format('dddd, MMM Do');
-    $("#currentDay").text(m);
+    var date = moment().format('dddd, MMM Do');
+    $("#currentDay").text(date);
 });
